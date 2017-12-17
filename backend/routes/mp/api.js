27822e02ea9eaa767215527a5239089d8ab69421
pge <pge@MacBook-Air-10.local>
2017-12-17 22:01:01 +0800

    verify mp signature

--- backend/routes/mp/api.js
+++ backend/routes/mp/api.js
@@ -1,8 +1,38 @@
+var crypto = require('crypto');
+var config = require('./../../../config')
 
 
 module.exports = {
     new_mp_message: function (req, res, context) {
         console.log(context);
         res.status(200).json('');
+    },
+    verify_signature: function (req, res, context) {
+        console.log(context);
+        var signature = req.query.signature;
+        var timestamp = req.query.timestamp;
+        var nonce = req.query.nonce;
+        var echostr = req.query.echostr;
+
+        /*  加密/校验流程如下： */
+        //1. 将token、timestamp、nonce三个参数进行字典序排序
+        var array = new Array();
+        array[0] = config.mp.token;
+        array[1] = timestamp;
+        array[2] = nonce;
+        array.sort();
+        var str = array.join('');
+
+        //2. 将三个参数字符串拼接成一个字符串进行sha1加密
+        var sha1Code = crypto.createHash("sha1");
+        var code = sha1Code.update(str,'utf-8').digest("hex");
+
+        //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+
+        if(code===signature){
+            res.send(echostr)
+        }else{
+            res.send("error");
+        }
     }
 };
--- backend/routes/mp/routes.js
+++ backend/routes/mp/routes.js
@@ -1,7 +1,11 @@
 var api = require('./api');
 
 module.exports = function(app, security) {
-    app.post('/api/mp/new_message', function(req, res){
+
+    app.get('/api/mp/new_message', function(req, res){
         api.new_mp_message(req, res, '>>>  received new mp message');
+    });
+    app.get('/api/mp/verify_signature', function (req, res) {
+        api.verify_signature(req, res, '>>> verify mp token url');
     });
 };
